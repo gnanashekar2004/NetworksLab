88f306864266cf13c39c03a79ebb84a496ad9432
Shekar2004 <gnanashekar2004@gmail.com>
2024-03-14 16:17:10 +0530

    modified the implementation of shared memory and semaphores

--- msocket.c
+++ msocket.c
@@ -6,25 +6,91 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <time.h>
 #include "msocket.h"
 
-extern semaphore semid1 = 0;
-extern semaphore semid2 = 0;
+void initialize_semaphores()
+{
+    key_t key1 = ftok(SEM_KEY_PATH, SEM_KEY_ID1);
+    if (key1 == -1)
+    {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
 
-SOCK_INFO sock_info = {0};
+    key_t key2 = ftok(SEM_KEY_PATH, SEM_KEY_ID2);
+    if (key2 == -1)
+    {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
 
-mtp_socket_t mtp_sockets[MAX_MTP_SOCKETS];
+    semid1 = semget(key1, 1, IPC_CREAT | IPC_EXCL | 0666);
+    if (semid1 == -1)
+    {
+        perror("semget");
+        exit(EXIT_FAILURE);
+    }
+
+    semid2 = semget(key2, 1, IPC_CREAT | IPC_EXCL | 0666);
+    if (semid2 == -1)
+    {
+        perror("semget");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void initialize_shared_memory(){
+    key_t key1=ftok(SHM_KEY_PATH,SHM_KEY_ID1);
+    if(key1==-1){
+        perror("ftok shared memory key1");
+        exit(EXIT_FAILURE);
+    }
+
+    key_t key2=ftok(SHM_KEY_PATH,SHM_KEY_ID2);
+    if(key2==-1){
+        perror("ftok shared memory key2");
+        exit(EXIT_FAILURE);
+    }
+
+    shmid_sock_info = shmget(key1,SHM_SOCK_INFO,IPC_CREAT|IPC_EXCL|0666);
+    if(shmid_sock_info==-1){
+        perror("shmget key1");
+        exit(EXIT_FAILURE);
+    }
+
+    shmid_mtp = shmget(key2,SHM_MTP_SOCKETS,IPC_CREAT|IPC_EXCL|0666);
+    if(shmid_mtp==-1){
+        perror("shmget key2");
+        exit(EXIT_FAILURE);
+    }
+
+    shared_memory_sock_info = shmat(shmid_sock_info,NULL,0);
+    if(shared_memory_sock_info==(void *)-1){
+        perror("shmat key1");
+        exit(EXIT_FAILURE);
+    }
+
+    shared_memory_mtp = shmat(shmid_mtp,NULL,0);
+    if(shared_memory_mtp==(void *)-1){
+        perror("shmat key2");
+        exit(EXIT_FAILURE);
+    }
+
+    sock_info = (SOCK_INFO *)shared_memory_sock_info;
+    shared_mtp_sockets = (mtp_socket_t *)shared_memory_mtp;
+}
 
 int m_socket(int domain, int type, int protocol)
 {
     int i, index = -1;
     for (i = 0; i < MAX_MTP_SOCKETS; i++)
     {
-        if (mtp_sockets[i].is_free == 1)
+        if (shared_mtp_sockets[i].is_free == 1)
         {
             index = i;
             break;
@@ -39,16 +105,16 @@ int m_socket(int domain, int type, int protocol)
 
     memset(&sock_info, 0, sizeof(SOCK_INFO));
 
-    up(&semid1);
-    down(&semid2);
+    sem_post(&semid1);
+    sem_wait(&semid2);
 
-    if (sock_info.sock_id == -1)
+    if (sock_info->sock_id == -1)
     {
         errno = EIO;
         return -1;
     }
 
-    mtp_sockets[index].udp_socket_id = sock_info.sock_id;
+    shared_mtp_sockets[index].udp_socket_id = sock_info->sock_id;
 
     memset(&sock_info, 0, sizeof(SOCK_INFO));
 
@@ -57,15 +123,18 @@ int m_socket(int domain, int type, int protocol)
 
 int m_bind(int mtp_socket_id, struct sockaddr_in src_addr, struct sockaddr_in dest_addr)
 {
-    int udp_socket_id = mtp_sockets[mtp_socket_id].udp_socket_id;
+    int udp_socket_id = shared_mtp_sockets[mtp_socket_id].udp_socket_id;
 
-    SOCK_INFO sock_info = {udp_socket_id,src_addr.sin_addr.s_addr,src_addr.sin_port,0};
+    sock_info->sock_id = udp_socket_id;
+    memcpy(&sock_info->IP, &src_addr.sin_addr.s_addr, sizeof(sock_info->IP));
+    sock_info->port = src_addr.sin_port;
+    sock_info->Errno = 0;
 
-    up(&semid1);
-    down(&semid2);
+    sem_post(&semid1);
+    sem_wait(&semid2);
 
-    if(sock_info.sock_id==-1){
-        errno = sock_info.Errno;
+    if(sock_info->sock_id==-1){
+        errno = sock_info->Errno;
         return -1;
     }
 
@@ -76,19 +145,19 @@ int m_bind(int mtp_socket_id, struct sockaddr_in src_addr, struct sockaddr_in de
 
 int m_sendto(int mtp_socket_id, const void *message, size_t length, struct sockaddr_in dest_addr)
 {
-    if(mtp_socket_id<0 || mtp_socket_id>=MAX_MTP_SOCKETS || mtp_sockets[mtp_socket_id].is_free){
+    if(mtp_socket_id<0 || mtp_socket_id>=MAX_MTP_SOCKETS || shared_mtp_sockets[mtp_socket_id].is_free){
         errno = EBADF;
         return -1;
     }
 
-    if(memcmp(&dest_addr,&mtp_sockets[mtp_socket_id].dest_addr,sizeof(struct sockaddr_in))!=0){
+    if(memcmp(&dest_addr,&shared_mtp_sockets[mtp_socket_id].dest_addr,sizeof(struct sockaddr_in))!=0){
         errno = ENOTBOUND;
         return -1;
     }
 
     int i,index=-1;
     for(i=0;i<10;i++){
-        if(mtp_sockets[mtp_socket_id].send_buffer[i][0]==0){
+        if(shared_mtp_sockets[mtp_socket_id].send_buffer[i][0]==0){
             index = i;
             break;
         }
@@ -99,40 +168,40 @@ int m_sendto(int mtp_socket_id, const void *message, size_t length, struct socka
         return -1;
     }
 
-    memcpy(mtp_sockets[mtp_socket_id].send_buffer[index],message,length);
+    memcpy(shared_mtp_sockets[mtp_socket_id].send_buffer[index],message,length);
 
     return 0;
 }
 
 int m_recvfrom(int mtp_socket_id, void *message, size_t length, struct sockaddr_in *src_addr)
 {
-    if(mtp_sockets[mtp_socket_id].recv_buffer[0][0]==0)
+    if(shared_mtp_sockets[mtp_socket_id].recv_buffer[0][0]==0)
     {
         errno = ENOMSG;
         return -1;
     }
 
-    memcpy(message, mtp_sockets[mtp_socket_id].recv_buffer[0],length);
+    memcpy(message, shared_mtp_sockets[mtp_socket_id].recv_buffer[0],length);
     
     for(int i=0;i<4;i++){
-        memcpy(&mtp_sockets[mtp_socket_id].recv_buffer[i][0],&mtp_sockets[mtp_socket_id].recv_buffer[i+1][0],sizeof(mtp_sockets[mtp_socket_id].recv_buffer[i]));
+        memcpy(&shared_mtp_sockets[mtp_socket_id].recv_buffer[i][0],&shared_mtp_sockets[mtp_socket_id].recv_buffer[i+1][0],sizeof(shared_mtp_sockets[mtp_socket_id].recv_buffer[i]));
     }
 
-    memset(&mtp_sockets[mtp_socket_id].recv_buffer[4][0],0,sizeof(mtp_sockets[mtp_socket_id].recv_buffer[4]));
+    memset(&shared_mtp_sockets[mtp_socket_id].recv_buffer[4][0],0,sizeof(shared_mtp_sockets[mtp_socket_id].recv_buffer[4]));
 
-    src_addr->sin_addr.s_addr = mtp_sockets[mtp_socket_id].dest_addr.sin_addr.s_addr;
-    src_addr->sin_port = mtp_sockets[mtp_socket_id].dest_addr.sin_port;
+    src_addr->sin_addr.s_addr = shared_mtp_sockets[mtp_socket_id].dest_addr.sin_addr.s_addr;
+    src_addr->sin_port = shared_mtp_sockets[mtp_socket_id].dest_addr.sin_port;
 
     return 0;
 }
 
 int m_close(int mtp_socket_id)
 {
-    close(mtp_sockets[mtp_socket_id].udp_socket_id);
+    close(shared_mtp_sockets[mtp_socket_id].udp_socket_id);
 
-    mtp_sockets[mtp_socket_id].is_free = 1;
-    mtp_sockets[mtp_socket_id].udp_socket_id = -1;
-    memset(&mtp_sockets[mtp_socket_id].dest_addr, 0, sizeof(struct sockaddr_in));
+    shared_mtp_sockets[mtp_socket_id].is_free = 1;
+    shared_mtp_sockets[mtp_socket_id].udp_socket_id = -1;
+    memset(&shared_mtp_sockets[mtp_socket_id].dest_addr, 0, sizeof(struct sockaddr_in));
 
     return 0;
 }
--- msocket.h
+++ msocket.h
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <errno.h>
@@ -21,11 +22,24 @@
 
 #define ENOTBOUND 60
 
-#define P(s) semop(s, &pop, 1)
-#define V(s) semop(s, &vop, 1)
-
 #define T 5
 #define Prob 0.05
+
+#define SEM_KEY_PATH "./msocket.h"
+#define SEM_KEY_ID1 1234
+#define SEM_KEY_ID2 4321
+
+#define SHM_KEY_PATH "./msocket.h"
+#define SHM_KEY_ID1 5678
+#define SHM_KEY_ID2 8765
+
+semaphore semid1 = -1;
+semaphore semid2 = -1;
+
+int shmid_sock_info = -1;
+int shmid_mtp = -1;
+void *shared_memory_sock_info = NULL;
+void *shared_memory_mtp = NULL;
 
 typedef struct {
     int sock_id;
@@ -45,10 +59,17 @@ typedef struct{
     int rwnd[5];
 }mtp_socket_t;
 
+#define SHM_SOCK_INFO sizeof(SOCK_INFO)
+#define SHM_MTP_SOCKETS (MAX_MTP_SOCKETS*sizeof(mtp_socket_t))
+
 typedef int semaphore;
 
-extern mtp_socket_t mtp_sockets[MAX_MTP_SOCKETS];
+SOCK_INFO *sock_info;
 
+mtp_socket_t *shared_mtp_sockets;
+
+void initialize_semaphores();
+void initialize_shared_memory();
 int m_socket(int domain,int type, int protocol);
 int m_bind(int mtp_socket_id,struct sockaddr_in src_addr,struct sockaddr_in dest_addr);
 int m_sendto(int mtp_socket_id,const void *message,size_t length,struct sockaddr_in dest_addr);
@@ -56,4 +77,4 @@ int m_recvfrom(int mtp_socket_id,void *message,size_t length,struct sockaddr_in 
 int m_close(int mtp_socket_id);
 int dropMessage(float p);
 
-#endif
+#endif;
